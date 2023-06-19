bd5b1aa263a7c887e1ab93110ad690ce480004ad
Ben Burkert <ben@benburkert.com>
2023-06-19 00:49:52 -0400

    Re-join cookie headers split by http/2

--- lib/src/headers.rs
+++ lib/src/headers.rs
@@ -5,4 +5,16 @@ pub fn filter_outgoing_headers(headers: &mut HeaderMap) {
     // framing headers automatically, and do not allow guests to include them.
     headers.remove(header::CONTENT_LENGTH);
     headers.remove(header::TRANSFER_ENCODING);
+
+    // Re-join Cookie headers split by http/2: https://github.com/hyperium/hyper/issues/2528
+    let cookies = headers
+        .get_all(header::COOKIE)
+        .iter()
+        .map(|v| v.to_str().unwrap())
+        .collect::<Vec<&str>>();
+
+    if cookies.len() > 1 {
+        let cookie = header::HeaderValue::from_str(cookies.join("; ").as_str()).unwrap();
+        headers.insert(header::COOKIE, cookie);
+    }
 }
